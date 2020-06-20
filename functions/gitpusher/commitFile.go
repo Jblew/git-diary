31755894865455db8854766d91e01942905bf280
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-20 19:33:53 +0200

    feat(functions): Implement gitpusher.commitFile

---
+++ functions/gitpusher/commitFile.go
@@ -0,0 +1,35 @@
+package gitpusher
+
+import (
+	"time"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
+)
+
+// AmendFile appends to file and commits the changes
+func (gitpusher *GitPusher) commitFile(path string, commitMessage string) error {
+	err := gitpusher.appendToFile(path, commitMessage)
+	if err != nil {
+		return err
+	}
+
+	worktree, err := gitpusher.repo.Worktree()
+	if err != nil {
+		return err
+	}
+
+	_, err = worktree.Add(path)
+	if err != nil {
+		return err
+	}
+
+	_, err = worktree.Commit(commitMessage, &git.CommitOptions{
+		Author: &object.Signature{
+			Name:  gitpusher.Config.CommitName,
+			Email: gitpusher.Config.CommitEmail,
+			When:  time.Now(),
+		},
+	})
+	return nil
+}
