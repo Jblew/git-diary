7bc231f86abd09621139c1a81a7bf87361ee802b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-20 11:45:07 +0200

    ci(functions): ensure proper project id for deployment

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -5,12 +5,20 @@ set -e
 
 source ./functions.config.sh
 
+if [ -z "${GCP_PROJECT_ID}" ]; then
+    echo "GCP_PROJECT_ID env is not set"
+    exit 1
+fi
+
 if [ -z "${GCP_PROJECT_REGION}" ]; then
     echo "GCP_PROJECT_REGION env is not set"
     exit 1
 fi
 
+
 ./generate-config.sh
+
+gcloud config set project ${GCP_PROJECT_ID}
 
 gcloud functions deploy PublishEntry \
   --trigger-http \
