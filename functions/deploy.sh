c2ce7a4242b90a38e4f53c5cded1892c70af2a86
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-20 11:43:11 +0200

    ci(functions): remove unused vars from deployment script

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -5,11 +5,6 @@ set -e
 
 source ./functions.config.sh
 
-if [ -z "${GCP_PUBSUB_TOPIC}" ]; then
-    echo "GCP_PUBSUB_TOPIC env is not set"
-    exit 1
-fi
-
 if [ -z "${GCP_PROJECT_REGION}" ]; then
     echo "GCP_PROJECT_REGION env is not set"
     exit 1
