e90c144d6704661747fa7eb54ab0519e1bee1b3c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-20 14:01:27 +0200

    test(functions): HTTPRequest debugging

---
+++ functions/util/DebugHTTPRequest.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// DebugHTTPRequest formats http request data to string
+// Source: https://medium.com/doing-things-right/pretty-printing-http-requests-in-golang-a918d5aaa000
+func DebugHTTPRequest(r *http.Request) string {
+	var request []string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	return strings.Join(request, "\n")
+}
--- functions/util/SendJSONError.go
+++ functions/util/SendJSONError.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// SendJSONError sends JSON formatted error to http.ResponseWriter
 func SendJSONError(w http.ResponseWriter, errMessage string) {
 	_, err := fmt.Fprintf(w, "{\"error\":\"%s\"}", errMessage)
 	if err != nil {
