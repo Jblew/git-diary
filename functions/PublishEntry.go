531ad512b13f5508c93727c7890b2b73cd4cc532
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-06-20 14:02:26 +0200

    test(functions): debug http headers

--- functions/PublishEntry.go
+++ functions/PublishEntry.go
@@ -3,9 +3,13 @@ package functions
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/Jblew/git-diary/functions/util"
 )
 
 // PublishEntry publishes entry to git repo
 func PublishEntry(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Test")
+	fmt.Fprintln(w, util.DebugHTTPRequest(r))
+	// application.FirebaseAuth.GetUser()
+	// fmt.Fprintln(w, "Test")
 }
