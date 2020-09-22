dcf1e049802c447288b416657feb4a723fe423a0
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-09-22 17:20:34 +0200

    feat: implement timetracking

---
+++ ui/public/src/features/timetracking/TimetrackingCurrent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Current</p>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TimetrackingList from './TimetrackingList.vue';
+import TimetrackingAdd from './TimetrackingAdd.vue';
+
+@Component
+export default class extends Vue {
+}
+</script>
+
+<style scoped>
+</style>
---
+++ ui/public/src/features/timetracking/TimetrackingPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <authenticated-header />
+
+    <timetracking-panel />
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue, Inject } from 'vue-property-decorator';
+import TimetrackingPanel from './TimetrackingPanel.vue';
+import { AuthenticatedHeader } from '@/components';
+import HomeHeader from './HomeHeader.vue';
+
+@Component({
+  components: {
+    TimetrackingPanel,
+    AuthenticatedHeader,
+  },
+})
+export default class TimetrackingPage extends Vue {
+  @Inject()
+  public readonly firebase!: firebase.app.App;
+
+  public user: firebase.User = this.firebase.auth().currentUser!;
+}
+</script>
+
+<style scoped>
+</style>
